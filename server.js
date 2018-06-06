e3ade0900d68bf59e6ced76c492923d4981c1695
Jason Roundtree <jasontree123@gmail.com>
2018-06-05 20:56:38 -0700

    update local port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 3000;
 
 app.get('/api/*', (req, res) => {
     res.json({ok: true})
