0b35bd54be01bd8322f656a1b60cbf9e09c1ffd0
Jason Roundtree <jasontree123@gmail.com>
2018-06-20 14:33:57 -0700

    Remove notes file

--- test/test-server.js
+++ test/test-server.js
@@ -1,19 +1,97 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const mongoose = require('mongoose');
+const faker = require('faker');
 
-const { app } = require('../server');
+const {app, runServer, closeServer} = require('../server');
+const { League, Round, Player, PointWeight, PointAllocation } = require('../models');
 
-const should = chai.should();
+const {TEST_DATABASE_URL} = require('../config');
+
+const expect = chai.expect();
 
 chai.use(chaiHttp);
 
-describe('API', function() {
-    it('Should return 200 on GET requests', function() {
-        return chai.request(app)
-            .get('/api/fooo')
-            .then(function(res) {
-                res.should.have.status(200);
-                res.should.be.json;
-            });
+function tearDownDb() {
+    console.warn('Tearing down database');
+    return mongoose.connection.dropDatabase();
+}
+
+describe('App API', function() {
+    before(() => {
+        return runServer(TEST_DATABASE_URL);
     });
+    beforeEach(() => {
+        return seedData();
+    });
+    afterEach(() => {
+        return tearDownDb();
+    });
+    after(() => {
+        return closeServer();
+    });
+
+    describe('POST new league endpoint', function() {
+        it('should add a new league', function() {
+            // TODO: complete newLeague
+            const newLeague = {
+                name: 'The Tour',
+                endDate: '10/1/2018',
+                pointTypes: ['First', 'Second', 'Birdie']
+            }
+            return chai.request(app)
+                .post('/league')
+                .send(newLeague)
+                .then(function(res) {
+                    expect(res).to.have.status(201);
+                    expect(res).to.be.json;
+                    expect(res.body).to.be.a('object');
+                    expect(res.body).to.include.keys(
+                        '_id', 'name', 'endDate', 'pointTypes'
+                    );
+                    expect(res.body.pointTypes).to.be.a('array');
+                    expect(res.body.name).to.equal(newLeague.name);
+                });
+        });
+    }); 
+
+    describe('PUT league endpoint', function() {
+        it('should update fields', function() {
+            const updatedData = {
+                name: 'Robot League',
+                endDate: '9/30/2118',
+                pointTypes: ['bleep', 'bloop', 'bizzz']
+            }
+            return League
+                .findOne()
+                .then(function(league) {
+                    updatedData.id = league._id;
+                    return chai.request(app)
+                        .put(`leagues/${league.id}`)
+                        .send(updatedData)
+                })
+                .then(function(res) {
+                    expect(res).to.have.status(204);
+                    return League.findById(updatedData.id)
+                })
+                .then(function(res) {
+                    expect(league.name).to.equal(updatedData.name);
+                    expect(league.endDate).to.equal(updatedData.endDate);
+                    expect(league.pointTypes).to.include(updatedData.pointTypes);
+                });
+        });
+    }); 
+
+    describe('GET league endpoint', function() {
+        it('should return a league', function() {
+           
+        })
+    })
+
+    describe('DELETE players endpoint', function() {
+        it('should delete a player', function() {
+           
+        })
+    })
+
 });
